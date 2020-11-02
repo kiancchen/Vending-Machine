7fddacb4d337aed9a85a482e99b375a268f797a2
Zexuan Long <zlon4018@uni.sydney.edu.au>
2020-11-02 15:55:40 +0800

    fix bug

--- src/test/java/VendingMachine/MainProcessorTest.java
+++ src/test/java/VendingMachine/MainProcessorTest.java
@@ -1,9 +1,29 @@
 package VendingMachine;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import static org.junit.Assert.*;
+
 public class MainProcessorTest {
+
+    @Before
+    @After
+    public void restoreResources(){
+        try {
+            Files.copy(new File("src/main/resources/user_backup.json").toPath(),
+                    new File("src/main/resources/user.json").toPath(),
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Test
     public void testMainProcessorConstructor() {
@@ -32,8 +52,7 @@ public class MainProcessorTest {
     @Test
     public void testRemoveUser() throws IOException{
         MainProcessor mainProcessor = new MainProcessor();
-        mainProcessor.addUser("test1","test1");
-        assertTrue(mainProcessor.removeUser(3));
+        assertTrue(mainProcessor.removeUser(2));
         assertFalse(mainProcessor.removeUser(10000));
 
     }
