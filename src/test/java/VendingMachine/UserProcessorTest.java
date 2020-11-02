d697f1620d519e2dfbb37305efa23043704e1fd7
Zexuan Long <zlon4018@uni.sydney.edu.au>
2020-11-02 14:33:56 +0800

    UserProceesorSignUp

--- src/test/java/VendingMachine/UserProcessorTest.java
+++ src/test/java/VendingMachine/UserProcessorTest.java
@@ -33,4 +33,18 @@ public class UserProcessorTest {
         assertFalse(userProcessor.verifyUser("blan","1234"));
 
     }
+
+    @Test
+    public void testAddUser() throws IOException {
+        UserProcessor userProcessor = new UserProcessor();
+        userProcessor.addUser("test","test");
+        assertTrue(userProcessor.verifyUser("test","test"));
+    }
+
+    @Test
+    public void testHasUser() throws IOException{
+        UserProcessor userProcessor = new UserProcessor();
+        assertTrue(userProcessor.hasUser("alan"));
+        assertFalse(userProcessor.hasUser("anyone"));
+    }
 }
