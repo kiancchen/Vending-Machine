21fc4699d787fb80292c285da5a75259390c3947
Cheng Chen <cche7346@uni.sydney.edu.au>
2020-11-09 16:03:20 +0800

    product

--- src/main/java/VendingMachine/App.java
+++ src/main/java/VendingMachine/App.java
@@ -1,8 +1,13 @@
 package VendingMachine;
 
+import VendingMachine.Data.Product;
+import VendingMachine.Processor.MainProcessor;
+import VendingMachine.Processor.ProductProcessor;
 import VendingMachine.Window.MainWindow;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -10,7 +15,7 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage){
         MainWindow mainWindow = new MainWindow();
         primaryStage.setScene(mainWindow.getScene());
         primaryStage.setTitle("Vending Machine");
--- src/main/java/VendingMachine/Processor/ProductProcessor.java
+++ src/main/java/VendingMachine/Processor/ProductProcessor.java
@@ -4,33 +4,92 @@ import VendingMachine.Data.Product;
 import VendingMachine.DatabaseHandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductProcessor {
-    private List<Product> products;
+    private Map<Product.Category, List<Product>> productMap;
 
     public ProductProcessor() throws IOException {
-        products = DatabaseHandler.loadProductData();
+        productMap = new HashMap<>();
+        productMap.put(Product.Category.DRINK, new ArrayList<>());
+        productMap.put(Product.Category.CHOCOLATE, new ArrayList<>());
+        productMap.put(Product.Category.CHIP, new ArrayList<>());
+        productMap.put(Product.Category.CANDY, new ArrayList<>());
     }
 
-    public boolean addProduct(Product.Category category, String name, double price, int quantity) {
+    public boolean addProduct(String category, String name, double price, int quantity) throws IOException {
+        List<Product> products = productMap.get(Product.Category.valueOf(category));
+        for (Product product : products) {
+            if (product.getName().equals(name)) {
+                return false;
+            }
+        }
+        products.add(new Product(Product.Category.valueOf(category), name, price, quantity));
+        DatabaseHandler.saveProductData(productMap);
+        return true;
+    }
+
+    public boolean setProductQuantity(String category, int code, int quantity) throws IOException {
+        if (quantity > 15){
+            return false;
+        }
+
+        List<Product> products = productMap.get(Product.Category.valueOf(category));
+        for (Product product : products) {
+            if (product.getCode() == code) {
+                product.setQuantity(quantity);
+                DatabaseHandler.saveProductData(productMap);
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean setProductQuantity(int code, int quantity) {
+    public boolean setProductName(String category, int code, String newName) throws IOException {
+        List<Product> products = productMap.get(Product.Category.valueOf(category));
+        for (Product product : products) {
+            if (product.getCode() == code) {
+                product.setName(newName);
+                DatabaseHandler.saveProductData(productMap);
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean setProductName(int code, String newName) {
+    public boolean setProductCategory(String category, int code, String newCategory) throws IOException {
+        List<Product> products = productMap.get(Product.Category.valueOf(category));
+        for (Product product : products) {
+            if (product.getCode() == code) {
+                // remove from the old category
+                products.remove(product);
+                product.setCategory(Product.Category.valueOf(newCategory));
+                // add to the new category
+                products = productMap.get(Product.Category.valueOf(newCategory));
+                products.add(product);
+                DatabaseHandler.saveProductData(productMap);
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean setProductCategory(int code, String category) {
+    public boolean setProductPrice(String category, int code, double price) throws IOException {
+        List<Product> products = productMap.get(Product.Category.valueOf(category));
+        for (Product product : products) {
+            if (product.getCode() == code) {
+                product.setPrice(price);
+                DatabaseHandler.saveProductData(productMap);
+                return true;
+            }
+        }
         return false;
     }
 
-    public boolean setProductPrice(int code, double price) {
-        return false;
+    public Map<Product.Category, List<Product>> getProductMap() {
+        return productMap;
     }
-
 }
