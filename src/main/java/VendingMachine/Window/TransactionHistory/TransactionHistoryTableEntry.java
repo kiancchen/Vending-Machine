3f0be51d81218552cdad65567375b328f7d10120
Cheng Chen <cche7346@uni.sydney.edu.au>
2020-11-19 19:36:41 +0800

    fix bugs

--- src/main/java/VendingMachine/Window/TransactionHistory/TransactionHistoryTableEntry.java
+++ src/main/java/VendingMachine/Window/TransactionHistory/TransactionHistoryTableEntry.java
@@ -1,17 +1,27 @@
 package VendingMachine.Window.TransactionHistory;
 
-public class TransactionHistoryTableEntry {
+import VendingMachine.Data.Product;
 
+import java.util.Map;
+
+public class TransactionHistoryTableEntry {
     String time;
     String moneyPaid;
     String changes;
     String paymentMethod;
+    private Map<Product, Integer> shoppingList;
 
-    public TransactionHistoryTableEntry(String time, String moneyPaid, String changes, String paymentMethod) {
+    public TransactionHistoryTableEntry(String time, String moneyPaid, String changes,
+                                        String paymentMethod, Map<Product, Integer> shoppingList) {
         this.changes = changes;
         this.time = time;
         this.moneyPaid = moneyPaid;
         this.paymentMethod = paymentMethod;
+        this.shoppingList = shoppingList;
+    }
+
+    public Map<Product, Integer> getShoppingList() {
+        return shoppingList;
     }
 
     public String getTime() {
--- src/main/java/VendingMachine/Window/TransactionHistory/TransactionHistoryWindow.java
+++ src/main/java/VendingMachine/Window/TransactionHistory/TransactionHistoryWindow.java
@@ -3,6 +3,7 @@ package VendingMachine.Window.TransactionHistory;
 import VendingMachine.Data.Transaction;
 import VendingMachine.Processor.UserProcessor;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -38,19 +39,28 @@ public class TransactionHistoryWindow {
         viewButton.setLayoutX(250);
         viewButton.setPrefWidth(100);
         viewButton.setPrefHeight(30);
-        viewButton.setText("View");
+        viewButton.setText("View details");
         pane.getChildren().add(viewButton);
     }
 
     private void initBtnAction() {
-        viewButton.setOnAction(event -> new TransactionProductWindow());
+        viewButton.setOnAction(event -> {
+            if (this.table.getSelectionModel().isEmpty()) {
+                Alert alert =
+                        new Alert(Alert.AlertType.WARNING, "Please select a transaction");
+                alert.show();
+            }else{
+                new TransactionProductWindow(this.table.getSelectionModel().getSelectedItem());
+            }
+
+        });
     }
 
     private void initTable() {
         table = new TableView<>();
-        table.setLayoutX(50);
+        table.setLayoutX(40);
         table.setLayoutY(15);
-        table.setPrefWidth(500);
+        table.setPrefWidth(520);
         table.setPrefHeight(400);
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         pane.getChildren().add(table);
@@ -66,6 +76,7 @@ public class TransactionHistoryWindow {
             column.setCellValueFactory(new PropertyValueFactory<>(properties[i]));
             table.getColumns().add(column);
         }
+        table.getColumns().get(0).setPrefWidth(120);
         setTableData();
     }
 
@@ -75,11 +86,11 @@ public class TransactionHistoryWindow {
             if (transaction.getStatus() != Transaction.Status.PAID) {
                 continue;
             }
-            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MM-dd HH:mm");
-                table.getItems().add(new TransactionHistoryTableEntry(transaction.getDate().format(fmt),
-                        Double.toString(transaction.getPaidAmount()),
-                        Double.toString(transaction.getChange()),
-                        transaction.getPayment().toString()));
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            table.getItems().add(new TransactionHistoryTableEntry(transaction.getDate().format(fmt),
+                    Double.toString(transaction.getPaidAmount()),
+                    Double.toString(transaction.getChange()),
+                    transaction.getPayment().toString(), transaction.getShoppingList()));
         }
     }
 }
