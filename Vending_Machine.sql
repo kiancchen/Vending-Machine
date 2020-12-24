35b59b1015e6347ece24598b25d29a7c467c9666
Cheng Chen <cche7346@uni.sydney.edu.au>
2020-12-24 23:49:56 +0800

    Manage products using MySQL

--- Vending_Machine.sql
+++ Vending_Machine.sql
@@ -35,8 +35,38 @@ CREATE TABLE `Cash` (
 
 LOCK TABLES `Cash` WRITE;
 /*!40000 ALTER TABLE `Cash` DISABLE KEYS */;
-INSERT INTO `Cash` VALUES (0.05,5),(0.1,5),(0.2,5),(0.5,5),(1,5),(2,5),(5,5),(10,5),(20,5),(50,5),(100,5);
+INSERT INTO `Cash` VALUES (0.05,5),(0.1,5),(0.2,5),(0.5,4),(1,5),(2,5),(5,5),(10,6),(20,5),(50,5),(100,5);
 /*!40000 ALTER TABLE `Cash` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Product` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `category` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `stock` int DEFAULT NULL,
+  `sold` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Product`
+--
+
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+INSERT INTO `Product` VALUES (1,'1','DRINK','Mineral Water',1,7,0),(2,'2','DRINK','Sprite',2,7,0),(3,'3','DRINK','Coca cola',3,7,0),(4,'4','DRINK','Pepsi',2,7,0),(5,'5','DRINK','Juice',3,7,0),(6,'6','CHOCOLATE','Marsssss',9.5,9,1),(7,'7','CHOCOLATE','M&M',2,7,0),(8,'8','CHOCOLATE','Bounty',3,7,0),(9,'9','CHOCOLATE','Snickers',4,7,0),(10,'10','CHIP','Smiths',1,7,0),(11,'11','CHIP','Pringles',3,7,0),(12,'12','CHIP','Kettle',5,7,0),(13,'13','CHIP','Thins',2,7,0),(14,'14','CANDY','Mentos',1,7,0),(15,'15','CANDY','Sour Patch',3,7,0),(16,'16','CANDY','Skittles',2,7,0);
+/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -48,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-24 19:49:29
+-- Dump completed on 2020-12-24 23:49:30
